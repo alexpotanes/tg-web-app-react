8cdad50656f6f516c0b891e18b5bf45a8f281870
Alex Potantsev <potanes@yandex.ru>
2025-09-18 20:09:02 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
+  const { tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, [tg])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!.
-        </p>
-      </header>
+      <Header />
+      <Routes>
+        <Route index element={<ProductList />}/>
+        <Route path={'form'} element={<Form />}/>
+      </Routes>
     </div>
   );
 }
